4365b22839a49a39e385dbae782ef41f59775434
Petterr24 <pedroo.rodri7@gmail.com>
2023-02-12 18:20:05 +0100

    [TFM] First UserAuthorization version

--- UserAuthorization/UserAuthorization.sol
+++ UserAuthorization/UserAuthorization.sol
@@ -1,5 +1,131 @@
 pragma solidity ^0.8.0;
 
+// Smart Contract to store the privileges of those users that can create new Smart Contracts or issue new Certificates
 contract UserAuthorization {
+    // Singleton to allow only creating one Instance of this Smart Contract
+    address public s_UserAuthorization;
 
+    // Admin address
+    address public admin;
+
+    constructor() {
+        // Check if an instance of this Smart Contract already exists
+        require(s_UserAuthorization == address(0), "The Instance of this Smart Contract already exists");
+        
+        // Set the Instance address to the address of the contract
+        s_UserAuthorization = address(this);
+
+        // Set the Admin address to be the only one to register new users
+        admin = msg.sender;
+    }
+
+    // Data structure for users
+    struct User {
+        address userAddress;
+        uint8 privilegeLevel;
+    }
+
+    // Mapping from user address to user data
+    mapping (address => User) public users;
+
+    // Enum for different privilege levels
+    enum PrivilegeLevel {
+        NONE,
+        CERTIFICATION,
+        PART,
+        COLLECTION,
+        ADMIN
+    }
+
+    // Event for logging user authorization
+    event UserAuthorized(
+        address indexed userAddress,
+        uint8 privilegeLevel
+    );
+
+    // Event for logging privilege modifications
+    event NewUserPrivilege(
+        address indexed userAddress,
+        uint8 oldPrivilegeLevel,
+        uint8 newPrivilegeLevel
+    );
+
+    // Event for logging user deletion
+    event UserRemoved(
+        address indexed userAddress
+    );
+
+    // Authorizes a new user
+    function authorizeUser(
+        address _userAddress,
+        uint8 _privilegeLevel
+    ) public isAdmin isValidPrivilege(_privilegeLevel) {
+        // Checks if the user is already authorized
+        require(users[_userAddress].userAddress == address(0), "User is already created and authorized");
+
+        // Stores the user data
+        users[_userAddress] = User(_userAddress, _privilegeLevel);
+
+        // Emits the event for logging the user authorization
+        emit UserAuthorized(_userAddress, _privilegeLevel);
+    }
+
+    // Changes the user privileges
+    function changeUserPrivilege(
+        address _userAddress,
+        uint8 _oldPrivilegeLevel,
+        uint8 _newPrivilegeLevel
+    ) public isAdmin isAnExistingUser(_userAddress) isValidPrivilege(_newPrivilegeLevel) {
+        // Checks if the user exists
+        require(users[_userAddress].userAddress != address(0), "The user does not exist");
+
+        // Checks if the old privilege level matches with the stored privilege
+        require(users[_userAddress].userAddress == _userAddress 
+            && users[_userAddress].privilegeLevel == _oldPrivilegeLevel, "The provided old privilege does not match with the stored one");
+
+        // Stores the new Privilege level
+        users[_userAddress].privilegeLevel = _newPrivilegeLevel;
+
+        // Emits the event for logging the user authorization
+        emit NewUserPrivilege(_userAddress, _oldPrivilegeLevel, _newPrivilegeLevel);
+    }
+
+    // Removes an Authorized User
+    function removeAuthorizedUser(
+        address _userAddress
+    ) public isAdmin isAnExistingUser(_userAddress) {
+        users[_userAddress].userAddress = address(0);
+        users[_userAddress].privilegeLevel = uint8(PrivilegeLevel.NONE);
+
+        // Emits the event for logging the user removal
+        emit UserRemoved(_userAddress);
+    }
+
+    // Checks if a user has the minimum provided privileges
+    function isAuthorized(
+        address _userAddress,
+        uint8 _requiredPrivilegeLevel
+    ) public isAnExistingUser(_userAddress) view returns (bool) {
+        // Retrieves the user data
+        User storage user = users[_userAddress];
+
+        // Checks if the user is authorized and has the required privilege level
+        return user.privilegeLevel >= _requiredPrivilegeLevel;
+    }
+
+    modifier isAdmin() {
+        require(msg.sender == admin, "You are not authorized to perform this action!");
+        _;
+    }
+
+    modifier isValidPrivilege(uint8 _privilege) {
+        require(_privilege >= uint8(PrivilegeLevel.NONE) && _privilege <= uint8(PrivilegeLevel.ADMIN), "Invalid Privilege value");
+        _;
+    }
+
+    modifier isAnExistingUser(address _userAddress) {
+        // Checks if the user exists
+        require(users[_userAddress].userAddress != address(0), "The user does not exist!");
+        _;
+    }
 }
