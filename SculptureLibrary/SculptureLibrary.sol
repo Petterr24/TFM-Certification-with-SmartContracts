12be77f1c4d753652c86fbda93c916ed1f5250f5
Petterr24 <pedroo.rodri7@gmail.com>
2023-02-24 23:35:24 +0100

    [TFM] Sculpture Library Impl

---
+++ SculptureLibrary/SculptureLibrary.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library SculptureLibrary {
+
+    enum CategorizationLabel { 
+        AUTHORIZED_UNIQUE_WORK, 
+        AUTHORIZED_UNIQUE_WORK_VARIATION, 
+        AUTHORIZED_WORK, 
+        AUTHORIZED_MULTIPLE, 
+        AUTHORIZED_CAST, 
+        POSTHUMOUS_WORK_AUTHORIZED_BY_ARTIST, 
+        POSTHUMOUS_WORK_AUTHORIZED_BY_RIGHTSHOLDERS, 
+        AUTHORIZED_REPRODUCTION, 
+        AUTHORIZED_EXHIBITION_COPY, 
+        AUTHORIZED_TECHNICAL_COPY, 
+        AUTHORIZED_DIGITAL_COPY 
+    }
+    
+    enum ConservationLabel {
+        AUTHORIZED_RECONSTRUCTION,
+        AUTHORIZED_RESTORATION,
+        AUTHORIZED_EPHEMERAL_WORK
+    }
+
+    struct PersistentData {
+        string name;
+        string artist;
+        string criticalCatalogNumber;
+    }
+
+    struct MiscellaneousData {
+        uint256 date;
+        string technique;
+        string dimensions;
+        string location;
+        CategorizationLabel categorizationLabel;
+    }
+
+    struct EditionData {
+        bool edition;
+        string editionExecutor;
+        uint256 editionNumber;
+    }
+
+    struct ConservationData {
+        bool conservation;
+        ConservationLabel conservationLabel;
+    }
+}
