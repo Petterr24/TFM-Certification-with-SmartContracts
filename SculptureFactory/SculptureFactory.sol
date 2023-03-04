465729d11ca5a94b2b409d0fed5735019f9a4d40
Petterr24 <pedroo.rodri7@gmail.com>
2023-03-04 14:38:35 +0100

    [TFM] Change order

--- SculptureFactory/SculptureFactory.sol
+++ SculptureFactory/SculptureFactory.sol
@@ -4,93 +4,6 @@ pragma solidity ^0.8.0;
 import "../SculptureLibrary/SculptureLibrary.sol";
 import "../UserAuthorization/UserAuthorization.sol";
 
-contract SculptureFactory {
-
-    // Singleton to allow only creating one Instance of this Smart Contract
-    address private s_SculptureFactory;
-
-    // Stores the addresses of the deployed SC (records)
-    address[] private sculptures;
-
-    // UserAuthorization instance
-    UserAuthorization userAuthorizationInstance;
-
-    event SculptureCreated(
-        SculptureLibrary.PersistentData persistentData,
-        SculptureLibrary.MiscellaneousData miscData,
-        SculptureLibrary.EditionData editionData,
-        SculptureLibrary.ConservationData conservationData
-    );
-
-    event SculptureAddress(address sculpture);
-
-    constructor(address _userAuthorizationAddress) {
-        // Checks if an instance of this Smart Contract already exists
-        require(s_SculptureFactory == address(0), "The Instance of this Smart Contract already exists");
-
-        //TODO: checks if the address is a correct SC address
-        userAuthorizationInstance = UserAuthorization(_userAuthorizationAddress);
-
-        // Checks if the user to deploy this SC is an admin user
-        require(userAuthorizationInstance.isAdminUser(msg.sender), "You are not authorized to deploy this SC");
-
-        // Sets the Instance address to the address of the contract
-        s_SculptureFactory = address(this);
-    }
-
-    function createSculpture(
-        SculptureLibrary.PersistentData memory _persistentData,
-        SculptureLibrary.MiscellaneousData memory _miscData,
-        SculptureLibrary.EditionData memory _editionData,
-        SculptureLibrary.ConservationData memory _conservationData,
-        string memory _sculptureOwner
-    ) public payable returns (address) {
-        // Checks if the user is an Admin user
-        require(userAuthorizationInstance.isAuthorizedToCreate(msg.sender) == true, "Your are not authorized to create a record.");
-
-        require(parseSculptureData(_persistentData, _miscData, _editionData, _conservationData, _sculptureOwner) == true);
-
-        Sculpture newSculpture = new Sculpture{value: msg.value}(_persistentData, _miscData, _editionData, _conservationData, _sculptureOwner, address(userAuthorizationInstance), address(this));
-        address newSculptureAddress = address(newSculpture);
-
-        sculptures.push(newSculptureAddress);
-
-        emit SculptureCreated(_persistentData, _miscData, _editionData, _conservationData);
-
-        // Emit the new Sculpture address
-        emit SculptureAddress(newSculptureAddress);
-
-        return newSculptureAddress;
-    }
-
-    function isSculptureFactory(address addr) public view returns (bool) {
-        return addr == address(this);
-    }
-
-    function parseSculptureData(SculptureLibrary.PersistentData memory _persistentData,
-        SculptureLibrary.MiscellaneousData memory _miscData,
-        SculptureLibrary.EditionData memory _editionData,
-        SculptureLibrary.ConservationData memory _conservationData,
-        string memory _sculptureOwner
-    ) private pure returns (bool) {
-        require(SculptureLibrary.checkMaxStringLength(_persistentData.sculptureId) == true, "The Sculpture ID field exceeds the maximum string length!");
-        require(SculptureLibrary.checkMaxStringLength(_persistentData.name) == true, "The Sculpture name field exceeds the maximum string length!");
-        require(SculptureLibrary.checkMaxStringLength(_persistentData.artist) == true, "The Artits field exceeds the maximum string length!");
-        require(SculptureLibrary.checkMaxStringLength(_persistentData.criticalCatalogNumber) == true, "The Critical Catalog Number field exceeds the maximum string length!");
-        require(SculptureLibrary.isValidDate(_miscData.date) == true, "The Date field is wrong. Two different options are possible, example:'c.1990' for an aproximate date or just 1990!");
-        require(SculptureLibrary.checkMaxStringLength(_miscData.technique) == true, "The Technique field exceeds the maximum string length!");
-        require(SculptureLibrary.checkMaxStringLength(_miscData.dimensions) == true, "The Dimensions field exceeds the maximum string length!");
-        require(SculptureLibrary.checkMaxStringLength(_miscData.location) == true, "The Location field exceeds the maximum string length!");
-        require(SculptureLibrary.isCategorizationLabelValid(_miscData.categorizationLabel) == true, "The Categorizatoin Label is not a valid value!");
-        require(SculptureLibrary.checkMaxStringLength(_editionData.editionExecutor) == true, "The Edition Excutor field exceeds the maximum string length!");
-        require(SculptureLibrary.checkMaxStringLength(_editionData.editionNumber) == true, "The Edition Number field exceeds the maximum string length!");
-        require(SculptureLibrary.isConservationLabelValid(_conservationData.conservationLabel) == true, "The Conservation Label is not a valid value!");
-        require(SculptureLibrary.checkMaxStringLength(_sculptureOwner) == true, "The Sculpture Owner field exceeds the maximum string length!");
-
-        return true;
-    }
-}
-
 contract Sculpture {
     // UserAuthorization instance
     UserAuthorization userAuthorizationInstance;
@@ -243,5 +156,92 @@ contract Sculpture {
 
         lastChangeTimestamp = block.timestamp;
         emit SculptureUpdated(lastChangeTimestamp, msg.sender, updatedData);
+    }
+}
+
+contract SculptureFactory {
+
+    // Singleton to allow only creating one Instance of this Smart Contract
+    address private s_SculptureFactory;
+
+    // Stores the addresses of the deployed SC (records)
+    address[] private sculptures;
+
+    // UserAuthorization instance
+    UserAuthorization userAuthorizationInstance;
+
+    event SculptureCreated(
+        SculptureLibrary.PersistentData persistentData,
+        SculptureLibrary.MiscellaneousData miscData,
+        SculptureLibrary.EditionData editionData,
+        SculptureLibrary.ConservationData conservationData
+    );
+
+    event SculptureAddress(address sculpture);
+
+    constructor(address _userAuthorizationAddress) {
+        // Checks if an instance of this Smart Contract already exists
+        require(s_SculptureFactory == address(0), "The Instance of this Smart Contract already exists");
+
+        //TODO: checks if the address is a correct SC address
+        userAuthorizationInstance = UserAuthorization(_userAuthorizationAddress);
+
+        // Checks if the user to deploy this SC is an admin user
+        require(userAuthorizationInstance.isAdminUser(msg.sender), "You are not authorized to deploy this SC");
+
+        // Sets the Instance address to the address of the contract
+        s_SculptureFactory = address(this);
+    }
+
+    function createSculpture(
+        SculptureLibrary.PersistentData memory _persistentData,
+        SculptureLibrary.MiscellaneousData memory _miscData,
+        SculptureLibrary.EditionData memory _editionData,
+        SculptureLibrary.ConservationData memory _conservationData,
+        string memory _sculptureOwner
+    ) external payable returns (address) {
+        // Checks if the user is an Admin user
+        require(userAuthorizationInstance.isAuthorizedToCreate(msg.sender) == true, "Your are not authorized to create a record.");
+
+        require(parseSculptureData(_persistentData, _miscData, _editionData, _conservationData, _sculptureOwner) == true);
+
+        Sculpture newSculpture = new Sculpture{value: msg.value}(_persistentData, _miscData, _editionData, _conservationData, _sculptureOwner, address(userAuthorizationInstance), address(this));
+        address newSculptureAddress = address(newSculpture);
+
+        sculptures.push(newSculptureAddress);
+
+        emit SculptureCreated(_persistentData, _miscData, _editionData, _conservationData);
+
+        // Emit the new Sculpture address
+        emit SculptureAddress(newSculptureAddress);
+
+        return newSculptureAddress;
+    }
+
+    function isSculptureFactory(address addr) public view returns (bool) {
+        return addr == address(this);
+    }
+
+    function parseSculptureData(SculptureLibrary.PersistentData memory _persistentData,
+        SculptureLibrary.MiscellaneousData memory _miscData,
+        SculptureLibrary.EditionData memory _editionData,
+        SculptureLibrary.ConservationData memory _conservationData,
+        string memory _sculptureOwner
+    ) private pure returns (bool) {
+        require(SculptureLibrary.checkMaxStringLength(_persistentData.sculptureId) == true, "The Sculpture ID field exceeds the maximum string length!");
+        require(SculptureLibrary.checkMaxStringLength(_persistentData.name) == true, "The Sculpture name field exceeds the maximum string length!");
+        require(SculptureLibrary.checkMaxStringLength(_persistentData.artist) == true, "The Artits field exceeds the maximum string length!");
+        require(SculptureLibrary.checkMaxStringLength(_persistentData.criticalCatalogNumber) == true, "The Critical Catalog Number field exceeds the maximum string length!");
+        require(SculptureLibrary.isValidDate(_miscData.date) == true, "The Date field is wrong. Two different options are possible, example:'c.1990' for an aproximate date or just 1990!");
+        require(SculptureLibrary.checkMaxStringLength(_miscData.technique) == true, "The Technique field exceeds the maximum string length!");
+        require(SculptureLibrary.checkMaxStringLength(_miscData.dimensions) == true, "The Dimensions field exceeds the maximum string length!");
+        require(SculptureLibrary.checkMaxStringLength(_miscData.location) == true, "The Location field exceeds the maximum string length!");
+        require(SculptureLibrary.isCategorizationLabelValid(_miscData.categorizationLabel) == true, "The Categorizatoin Label is not a valid value!");
+        require(SculptureLibrary.checkMaxStringLength(_editionData.editionExecutor) == true, "The Edition Excutor field exceeds the maximum string length!");
+        require(SculptureLibrary.checkMaxStringLength(_editionData.editionNumber) == true, "The Edition Number field exceeds the maximum string length!");
+        require(SculptureLibrary.isConservationLabelValid(_conservationData.conservationLabel) == true, "The Conservation Label is not a valid value!");
+        require(SculptureLibrary.checkMaxStringLength(_sculptureOwner) == true, "The Sculpture Owner field exceeds the maximum string length!");
+
+        return true;
     }
 }
